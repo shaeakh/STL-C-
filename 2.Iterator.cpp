d30a942961721e0ddafb98edde9ae1ae711ffec7
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2023-04-02 19:19:09 +0600

    .

---
+++ 2.Iterator.cpp
@@ -0,0 +1,16 @@
+/*bismillahir~rahmanir~rahim*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {2, 3, 1, 5, 9, 7};
+    vector<int>::iterator it; // declaration
+
+    // without iterator_data_type access:
+
+    cout << *v.begin() << endl;
+
+    return 0;
+}
+/* problem link: */
